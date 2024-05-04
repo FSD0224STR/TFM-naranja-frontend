3c62f612f4b1b3188b1c08bc0c39490c8a00fa62
David <github.astound854@passfwd.com>
2024-05-04 18:08:28 +0200

    ajustando el header y funcionando los link del react router. Creado apiService para pruebas login y register

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import SignIn from "./components/SignIn";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Register from "./components/Register";
 import Login from "./components/Login";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Home />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import "./Header.css";
+import "./Header.css"
+import { Link } from "react-router-dom";
 
-const {Header} = Layout;
-
-const Header () => {
-    return (<div className="header" />
-    <Menu
-      theme="dark"
-      mode="horizontal"
-      defaultSelectedKeys={['2']}
-      items={items}
-      style={{
-        flex: 1,
-        minWidth: 0,
-      }}
-    />)
+export default function Header() {
+    return (
+        <header >
+            <nav className="header">
+                <Link to="/">Home</Link>
+                <Link to="/register">Register</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/profile">Profile</Link>
+              </nav>
+        </header>
+    );
 }
-
-export default {Header}
