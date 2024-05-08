1c97080bf39b9954c9d07456368db8295f8c5364
SanJas <creativehola@gmail.com>
2024-05-08 21:08:21 +0200

    Hassan Footer in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
+      <Footer />
     </>
   );
 }
