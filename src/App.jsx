236b849dd650cdc52bfd4eebc3327af2d81524fe
Cristian <cristiangarciaibanez95@gmail.com>
2024-05-17 19:39:32 +0200

    commit implementacion logout y captcha

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/LogContext";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
@@ -11,18 +12,20 @@ import Product from "./components/Product";
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/addProduct" element={<AddProduct />} />
-        <Route path="/listProducts" element={<ListProducts />} />
-        <Route path="/product/:id" element={<Product />} />
-      </Routes>
-      <Footer />
-    </>
+    <AuthProvider>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/addProduct" element={<AddProduct />} />
+          <Route path="/listProducts" element={<ListProducts />} />
+          <Route path="/product/:id" element={<Product />} />
+        </Routes>
+        <Footer />
+      </>
+    </AuthProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-import LogOut from "./LogOut";
+import { useAuth } from "../context/LogContext";
 
-const Header = ({ isLoggedIn }) => {
+const Header = () => {
+  const { isLoggedIn, logout } = useAuth();
+
   return (
     <header>
       <nav className="header">
         <Link to="/home">Home</Link>
-        {!isLoggedIn && (
+        {isLoggedIn ? (
+          <>
+            <Link to="/profile">Profile</Link>
+            <Link to="/addProduct">Add Product</Link>
+            <Link to="/listProducts">List Products</Link>
+            <Link onClick={logout} to="/login" >Logout</Link>
+          </>
+        ) : (
           <>
             <Link to="/register">Register</Link>
             <Link to="/login">Login</Link>
           </>
         )}
-        {isLoggedIn && <LogOut />}
-        <Link to="/profile">Profile</Link>
-        <Link to="/addProduct">addProduct</Link>
-        <Link to="/listProducts">ListProducts</Link>
       </nav>
     </header>
   );
 };
 
 export default Header;
-
