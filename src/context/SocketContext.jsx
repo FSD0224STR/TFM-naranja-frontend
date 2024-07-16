ef81cd3d0e88a895a95cd04bac25a22346c8dd09
David Aranda <david.aranda@gmail.com>
2024-07-16 21:45:05 +0200

    revirtiendo console.log

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -19,7 +19,6 @@ export const SocketContextProvider = ({ children }) => {
 
   // Eventos en espera a ser llamados desde el backend para actualizar el listado de mensajes con los nuevos que llegan
   useEffect(() => {
-    console.log("import.meta.env.VITE_BACKEND: ", import.meta.env.VITE_BACKEND);
     socket.current = io(import.meta.env.VITE_BACKEND);
 
     socket.current.on("privateMessage", (msg, typeUser) => {
